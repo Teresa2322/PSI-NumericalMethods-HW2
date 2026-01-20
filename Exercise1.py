f191cb448aa1ddc4c9bc61fb7dd1fece5abffd31
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-19 22:05:56 -0500

    working model

--- Exercise1.py
+++ Exercise1.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 E = 1
 m = 1 
 def U(x):
@@ -9,24 +9,32 @@ def VHO(x,omega):
         return (x**2)*m*(omega**2)/2
 
 def EvolM(x):
-	return np.array([[0,1],[-2*m*(U(x)-E),0]])
+	return np.array([[0,1],[2*m*(U(x)-E),0]])
 
 def der(x, y):
 	return EvolM(x)@y
 
-def RK4(x_i,x_f, y_i, h, der): #this f is the defivative function particularly
-        Nits = int((x_f - x_i)/h)
-        x_n = x_i
-        y_n = np.array(y_i)
-        for i in range(1, Nits + 1):
-                k1 = der(x_n, y_n)
-                k2 = der(x_n + h/2, y_n + h*k1/2)
-                k3 = der(x_n + h/2, y_n + h*k2/2)
-                k4 = der(x_n + h, y_n + h*k3)
-                y_n = y_n + (h/6)*(k1 + 2*k2 + 2*k3 +k4)
-                x_n += h
-        return y_n
+def RK4(x_i,x_f, y_i, h, der):
+	Nits = int((x_f - x_i)/h)
+	x_n = x_i
+	y_n = np.array(y_i)
+	x_array = []
+	psi_array = []
+	for i in range(1, Nits + 1):
+		k1 = der(x_n, y_n)
+		k2 = der(x_n + h/2, y_n + h*k1/2)	
+		k3 = der(x_n + h/2, y_n + h*k2/2)
+		k4 = der(x_n + h, y_n + h*k3)
+		y_n = y_n + (h/6)*(k1 + 2*k2 + 2*k3 +k4)
+		psi_array.append(y_n[0])
+		x_n += h
+		x_array.append(x_n)
+	return x_array, psi_array
 
 print("trial run:", RK4(0, 1, [0, 1], 0.01, der))
 
+plt.plot(RK4(0, 10, [0, 1], 0.0001, der)[0], RK4(0, 10, [0, 1], 0.0001, der)[1])
+plt.show()
 N = 1000 #number of steps
+
+
