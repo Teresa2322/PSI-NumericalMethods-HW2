40551d475f9abd10ff6ff1472fa02e5776708606
Teresa2322 <teresadelaguila23@gmail.com>
2026-01-22 14:50:23 -0500

    fig1

--- Exercise1.py
+++ Exercise1.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-E = 4
-m = 1.79 
+
+m = 1
+
 def U(x):
-        return 0
+        return 0 #(x**2)*m*(omega**2)/2 for Harmonic
 
 def VHO(x,omega):
         return (x**2)*m*(omega**2)/2
 
-def EvolM(x,E):
+def EvolM(x,E): #chose to not make it a function of U(x)
 	return np.array([[0,1],[2*m*(U(x)-E),0]])
 
 def der(x, y,E):
@@ -31,21 +32,30 @@ def RK4(x_i,x_f, y_i, h, der,E):
 		x_array.append(x_n)
 	return x_array, psi_array
 
-print("trial run:", RK4(0, 1, [0, 1], 0.01, der, E))
+#E st psi(10) approx 0 
+#I know there is some energy between 0.4 and 0.5 that satisfies this
 
-plt.plot(RK4(0, 10, [0, 1], 0.001, der, 1)[0], RK4(0, 10, [0, 1], 0.001, der, 1)[1])
+tol = 10**(-5)
+a = 0.4
+b = 0.5
+
+def RK4fE(E):
+	return RK4(0, 10, [0, 1], 0.001, der, E)[1][-1]
+
+while (b - a)/2 > tol:
+	c = (a + b)/2
+	if  RK4fE(c) == 0:
+		break
+	elif   RK4fE(a)* RK4fE(c) < 0:
+		b = c
+	elif   RK4fE(b)* RK4fE(c) < 0:
+          	a = c
+	print("Root found at:",c," with psi at that value being", RK4fE(c))
+
+plt.plot(RK4(0, 10, [0, 1], 0.001, der, c)[0], RK4(0, 10, [0, 1], 0.001, der, c)[1])
+plt.xlabel("x")
+plt.ylabel("psi(x)")
 plt.show()
-
-y = 1
-e_i = 1
-
-while abs(y) > 10**(-4):
-	y = RK4(0, 10, [0, 1], 0.001, der, e_i)[1][-1]
-	e_i += 0.001
-#this is a silly and inefficient approach of course
-print("outputted y is", y, "and corresponding energy is", e_i)	
-#Finding E st psi(10) approx 0 
-
-
-
+plt.savefig('Exercise1Ca.png', dpi=300)
+plt.close()
 
